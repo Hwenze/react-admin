d162889d721efbf645d294c8e063337e5e6d7a18
Joker <hongkai.fan>
2020-02-27 17:50:05 +0800

    更改为HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import Layout from './pages/Layout';
 import { GlobalStyle } from './style';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,HashRouter } from 'react-router-dom';
 import 'antd/dist/antd.css';
 function App() {
   return (
-    <BrowserRouter>
+    // BrowserRouter 本地测试
+    <HashRouter>
       <div className="App">
         <GlobalStyle/>
         <Layout />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
